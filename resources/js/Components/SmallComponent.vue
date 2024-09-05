b188966da0171725d9c028337501a510a4cda42b
Kyle Knowles <kknowles@springfieldclinic.com>
2024-09-05 12:12:20 -0500

    feat: added Filament Notification button to SmallComponent.vue

--- resources/js/Components/SmallComponent.vue
+++ resources/js/Components/SmallComponent.vue
@@ -1,3 +1,23 @@
+<script setup>
+import { ref } from "vue";
+function notify() {
+    const notification = new window.FilamentNotification()
+        .title(`Hello Filament Notification from Vue`)
+        .success()
+        .send();
+}
+const clicked = ref(0);
+</script>
 <template>
-    <h4 class="text-red-500 border-2 border-green-500 px-2 py-2">Small Component</h4>
+    <div class="flex space-x-2">
+        <h4 class="px-2 py-2 text-red-500 border-2 border-green-500">
+            Small Component
+        </h4>
+        <button
+            @click="notify"
+            class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+        >
+            Filament Notification
+        </button>
+    </div>
 </template>
