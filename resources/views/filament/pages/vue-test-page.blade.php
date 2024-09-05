85047f2bd9cf27e3932ccde825666fb7d921c89e
Chris Reed <chris@reedtech.us>
2024-09-05 11:58:47 -0500

    fix: 🐛 added `wire:ignore` attribute to `div` in blade template
    Ensured Vue component in Livewire wrapper is not re-rendered by Livewire.

--- resources/views/filament/pages/vue-test-page.blade.php
+++ resources/views/filament/pages/vue-test-page.blade.php
@@ -1,7 +1,7 @@
 <x-filament-panels::page>
     <p>This is the Livewire page wrapping the Vue Components.</p>
 
-    <div class="flex flex-row w-full border border-red-500 p-4 space-y-2 space-2">
+    <div wire:ignore class="flex flex-row w-full border border-red-500 p-4 space-y-2 space-2">
         {{--<livewire:vue-component component="TestComponent" :props="$props" />--}}
         <livewire:vue-component component="TestComponent" />
         <livewire:vue-component component="TestComponent" :props="['type' => 'user']" />
